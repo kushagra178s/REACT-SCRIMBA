628a6735aa8ed01ace6ed2a7e65e5d5a8938f67b
KSX <kushagra17800@gmail.com>
2022-08-05 06:56:08 +0530

    5/8/22

---
+++ reactsite/reactapp1/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="https://uploads.sitepoint.com/wp-content/uploads/2017/04/1493235373large_react_apps_A-01.png" alt="react image" width="150px" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
