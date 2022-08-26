8bb6979d409316ecee7f128f965c081476c0fffb
KSX <kushagra17800@gmail.com>
2022-08-26 23:12:17 +0530

    26/8/22

--- meme generator/meme-generator/src/App.js
+++ meme generator/meme-generator/src/App.js
@@ -1,8 +1,9 @@
 import React from "react"
-
+import Header from "./components/Header"
+import "./style.css"
 /**
  * Challenge: Build the Header component
  */
 export default function App() {
-    return <h1>Hello world!</h1>
+    return <Header/>
 }
---
+++ meme generator/meme-generator/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header () {
+    return (
+        <header>
+        <img 
+        src = {require("../images/troll-face.png")} 
+        className="header--image"
+        />
+        <h2 className="header--title">Header Component</h2>
+        <h4 className="header--project">React Course Project Meme Genrator</h4>
+        </header>
+    )
+}
